f352a75fc50e6449bf9c03a299e6cd5d7b49df8b
George <george9040@abv.bg>
2022-05-13 18:00:50 +0300

    Added search

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
-import axios from 'axios'
-
+import "./App.css";
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import Search from "./components/users/Search";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+  };
 
+  // async componentDidMount() {
+  //   this.setState({loading: true})
+  //   const res = await axios.get(`https://api.github.com/users?client_id=e2b30149ffdffa0c63f5&client_secret=21474ecec82d398cd6f570427579d6dd6aa37934`)
+  //   this.setState({users: res.data, loading: false})
+  // }
 
-  async componentDidMount() {
-    this.setState({loading: true})
-    const res = await axios.get(`https://api.github.com/users?client_id=e2b30149ffdffa0c63f5&client_secret=a87fc628763c61de80e731aa4e31d4a52d997635`)
-    this.setState({users: res.data, loading: false})
-  }
-  
-    
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=e2b30149ffdffa0c63f5&client_secret=a87fc628763c61de80e731aa4e31d4a52d997635`
+    );
+    console.log(res.data.items);
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
-
     return (
       <div className="App">
-      <Navbar title='GitHub Finder' icon="fba fa-github"/>
-      <div className='container'>
-      <Users loading={this.state.loading} users={this.state.users} />
+        <Navbar title="GitHub Finder" icon="fba fa-github" />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-      </div>
-    )
-  };
-  
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+
+   this.props.searchUsers(this.state.text);
+   this.setState({text: ''})
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+        <>
+      <form onSubmit={this.onSubmit.bind(this)} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search users..."
+          onChange={this.onChange}
+          value={this.state.text}
+        />
+        <input
+          type="submit"
+          vlaue="Serach"
+          className="btn btn-dark btn-block"
+          />
+      </form>
+      {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
+    </>
+    );
+  }
+}
+
+export default Search;
